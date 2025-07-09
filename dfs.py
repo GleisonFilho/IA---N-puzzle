9cacc2659731c9ff854f101eabd08af65fbc6d42
Widiney Veloso <vwidiney@gmail.com>
2025-07-08 23:48:30 -0300

    Com limitador no DFS

--- dfs.py
+++ dfs.py
@@ -10,32 +10,39 @@ def gerar_sucessores(estado):
     linha, coluna = encontrar_zero(estado)
     movimentos = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     sucessores = []
+
     for dx, dy in movimentos:
         nova_linha, nova_coluna = linha + dx, coluna + dy
         if 0 <= nova_linha < len(estado) and 0 <= nova_coluna < len(estado[0]):
             novo_estado = deepcopy(estado)
-            novo_estado[linha][coluna], novo_estado[nova_linha][nova_coluna] = novo_estado[nova_linha][nova_coluna], novo_estado[linha][coluna]
+            novo_estado[linha][coluna], novo_estado[nova_linha][nova_coluna] = \
+                novo_estado[nova_linha][nova_coluna], novo_estado[linha][coluna]
             sucessores.append(novo_estado)
+
     return sucessores
 
-def dfs(inicial, objetivo, limite=50):
+def estado_para_string(estado):
+    return ''.join(str(num) for linha in estado for num in linha)
+
+def dfs(inicial, objetivo, limite=30):
+    pilha = [(inicial, [], 0)]  # (estado, caminho, profundidade)
     visitados = set()
-    pilha = [(inicial, [])]
     nos_expandidos = 0
 
     while pilha:
-        atual, caminho = pilha.pop()
+        atual, caminho, profundidade = pilha.pop()
+
         if atual == objetivo:
             return caminho + [atual], nos_expandidos
 
-        chave = str(atual)
-        if chave in visitados or len(caminho) > limite:
+        chave = estado_para_string(atual)
+        if chave in visitados or profundidade > limite:
             continue
 
         visitados.add(chave)
         nos_expandidos += 1
 
         for sucessor in gerar_sucessores(atual):
-            pilha.append((sucessor, caminho + [atual]))
+            pilha.append((sucessor, caminho + [atual], profundidade + 1))
 
     return None, nos_expandidos
